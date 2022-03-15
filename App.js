159b39a897eb5dcf9b41bab46476e37c1ac60758
junior-x <marcosmansur@outlook.com>
2022-03-14 18:52:04 -0700

    baStyle

--- App.js
+++ App.js
@@ -4,70 +4,50 @@ import { View, Text, SafeAreaView } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 
-import Profile from "./src/pages/profile";
+import Dashboard from "./src/pages/dashboard";
 import NFT from "./src/pages/nft";
+import Profile from "./src/pages/profile";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Profile} />
-        <Tab.Screen name="Settings" component={NFT} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={{ 
+            headerShown: false, 
+            tabBarShowLabel:  false, 
+            tabBarStyle: {backgroundColor: '#23202e', elevation: 0}, 
+          }}>
+          <Tab.Screen name="Activity" component={Dashboard} />
+          <Tab.Screen name="NFT" component={NFT} />
+          <Tab.Screen name="Pofile" component={Profile} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 function HomeScreen() {
-  return(
-
-  <>
-    <SafeAreaView>
-      <View>
-        <Text>Home Screen</Text>
-      </View>
-    </SafeAreaView>
-  </>
+  return (
+    <>
+      <SafeAreaView>
+        <View>
+          <Text>Home Screen</Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 function SettingsScreen() {
-  return(
-
-  <>
-    <SafeAreaView>
-      <View>
-        <Text>Settings</Text>
-      </View>
-    </SafeAreaView>
-  </>
+  return (
+    <>
+      <SafeAreaView>
+        <View>
+          <Text>Settings</Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
-// import React from "react";
-// import { SafeAreaView, StatusBar } from "react-native";
-
-// import Dashboard from "./src/pages/dashboard";
-// import LogIn from "./src/pages/logIn";
-// import Profile from "./src/pages/profile";
-// import NFT from "./src/pages/nft";
-
-// import BottomNavigator from "./src/components/bottomNavigator";
-
-// export default function App() {
-
-//   return (
-//     <>
-//       <SafeAreaView>
-//         <StatusBar hidden />
-//         <LogIn />
-//         {/* <Dashboard /> */}
-//         {/* <Profile/> */}
-//         {/* <NFT /> */}
-//         {/* <BottomNavigator />  */}
-//       </SafeAreaView>
-//     </>
-//   );
-// }
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ React Native CLI App with Sass
  
  <p align="center"><em>Simple social media app about NFTs using React-Native CLI.</p>
 
- ![App Preview](/assets/print/07.jpg)
+ ![App Preview](/assets/print/08.jpg)
 .
 
 <p align="center"><em>Links para o SITE && PROJETO </p>
@@ -69,6 +69,11 @@ React Native CLI App with Sass
 <p align="center" float="left">npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view. </p>
 
  ![App Preview](/assets/print/07.jpg)
+
+----
+<p align="center" float="left">Bottom Nav Bar implementation.</p>
+
+ ![App Preview](/assets/print/08.jpg)
 
 ----
 <!-- <p align="center" float="left">função para passar as propriedades de cada informação. </p>
