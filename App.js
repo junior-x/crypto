6a7eabc4880d61c09b7b1142dc43af54094c1052
junior-x <marcosmansur@outlook.com>
2022-03-22 17:10:10 -0700

    numsei

--- App.js
+++ App.js
@@ -5,11 +5,28 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import LogIn from "./src/pages/logIn";
 import Dashboard from "./src/pages/dashboard";
+import { useEffect } from 'react/cjs/react.production.min';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  GET 
+
+  useEffect(() => {
+    fetch('https://api.nasa.gov/planetary/apod',{
+      method: 'GET',
+      headers: {
+        'Accept': 'application/jason'
+      }
+    }
+    )
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+  }, []) 
   return (
     <>
       <NavigationContainer>
