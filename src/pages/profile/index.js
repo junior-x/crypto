c1697373e5fa51b80ed213ce855120268c789c17
junior-x <marcosmansur@outlook.com>
2022-03-27 07:17:10 -0700

    profileStyles

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,10 +1,11 @@
-import { 
-    View, 
-    Text, 
-    Image, 
-    TouchableOpacity, 
-    ScrollView, 
-    SafeAreaView } from "react-native"
+import {
+    View,
+    Text,
+    Image,
+    TouchableOpacity,
+    ScrollView,
+    SafeAreaView
+} from "react-native"
 import React from "react";
 import LinearGradient from 'react-native-linear-gradient';
 import styles from "./../../styles/profileStyles.scss";
@@ -14,42 +15,55 @@ import PostImg from "./../../../assets/img/img01.png";
 import IconCheck from "./../../../assets/img/check.png";
 import IconProfile from "./../../../assets/img/icon.png";
 
+import PostImg2 from "./../../../assets/img/img02.png";
+import PostImg3 from "./../../../assets/img/img03.png";
+import PostImg4 from "./../../../assets/img/img04.png";
+import PostImg5 from "./../../../assets/img/img05.png";
+import PostImg6 from "./../../../assets/img/img06.png";
+
+import IconCard from "./../../../assets/img/iconCard.png";
+import IconGrid from "./../../../assets/img/iconGrid.png";
+
 export default function Profile() {
 
     return (
         <SafeAreaView>
             <View style={styles.div} >
-                <LinearGradient 
-                    style={styles.divProfile} 
-                    colors={['#1a1626', 'rgba(35, 32, 46, 1)']}
-                    start={{ x: 0, y: 0.60 }}
-                    end={{ x: 0, y: 0.81 }}
-                >
-                <Image source={PostImg} resizeMode='cover' style={styles.profilePostImage}></Image>
-                    <View style={stylesTopBar.topBarAbsolute}>
-                        <View style={stylesTopBar.iconDashboard} />
-                        <View style={stylesTopBar.iconBurger} />
-                    </View>
-                    <View style={styles.divProfileItems}>
-                        <TouchableOpacity style={styles.icon}>
-                            <Image source={IconProfile} style={styles.iconProfile} resizeMode='cover'></Image>
-                            {/* <View style={styles.iconCheck}></View> */}
-                            <Image source={IconCheck} style={styles.iconCheck}></Image>
-                        </TouchableOpacity>
-                        <Text style={styles.title}>Juniorx</Text>
-                        <Text style={styles.subtitle}>@drift3rx</Text>
-                    </View>
-                    <View style={styles.divFilter}>
-                        <TouchableOpacity style={styles.buttonFilter1}></TouchableOpacity>
-                        <TouchableOpacity style={styles.buttonFilter2}></TouchableOpacity>
-                    </View>
-                </LinearGradient>
+
                 <ScrollView>
+                    <View
+                        style={styles.divProfile}
+                    >
+                        <Image source={PostImg} resizeMode='cover' style={styles.profilePostImage}></Image>
+                        <LinearGradient
+                            style={styles.divProfileGradient}
+                            colors={['rgba(35, 32, 46, 0.1)', 'rgba(35, 32, 46, 1)']}
+                            start={{ x: 0, y: 0.39 }}
+                            end={{ x: 0, y: 0.84 }}
+                        />
+                        <View style={stylesTopBar.topBarAbsolute}>
+                            <View style={stylesTopBar.iconDashboard} />
+                            <View style={stylesTopBar.iconBurger} />
+                        </View>
+                        <View style={styles.divProfileItems}>
+                            <TouchableOpacity style={styles.icon}>
+                                <Image source={IconProfile} style={styles.iconProfile} resizeMode='cover'></Image>
+                                {/* <View style={styles.iconCheck}></View> */}
+                                <Image source={IconCheck} style={styles.iconCheck}></Image>
+                            </TouchableOpacity>
+                            <Text style={styles.title}>Juniorx</Text>
+                            <Text style={styles.subtitle}>@drift3rx</Text>
+                            <View style={styles.divFilter}>
+                                <TouchableOpacity style={styles.buttonFilter1}><Image source={IconCard} resizeMode='cover' style={styles.imagePostFilter}></Image></TouchableOpacity>
+                                <TouchableOpacity style={styles.buttonFilter2}><Image source={IconGrid} resizeMode='cover' style={styles.imagePostFilter}></Image></TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
                     <View style={styles.profilePost}>
                         <TouchableOpacity style={styles.buttonPost}>
                             <View>
                                 <View style={"#"}>
-                                    <Image>
+                                    <Image source={PostImg2} style={styles.imagePostProfile}>
 
                                     </Image>
                                 </View>
@@ -58,7 +72,7 @@ export default function Profile() {
                         <TouchableOpacity style={styles.buttonPost}>
                             <View>
                                 <View style={"#"}>
-                                    <Image>
+                                    <Image source={PostImg3} style={styles.imagePostProfile}>
 
                                     </Image>
                                 </View>
@@ -67,7 +81,7 @@ export default function Profile() {
                         <TouchableOpacity style={styles.buttonPost}>
                             <View>
                                 <View style={"#"}>
-                                    <Image>
+                                    <Image source={PostImg} style={styles.imagePostProfile}>
 
                                     </Image>
                                 </View>
@@ -76,7 +90,7 @@ export default function Profile() {
                         <TouchableOpacity style={styles.buttonPost}>
                             <View>
                                 <View style={"#"}>
-                                    <Image>
+                                    <Image source={PostImg4} style={styles.imagePostProfile}>
 
                                     </Image>
                                 </View>
@@ -85,7 +99,7 @@ export default function Profile() {
                         <TouchableOpacity style={styles.buttonPost}>
                             <View>
                                 <View style={"#"}>
-                                    <Image>
+                                    <Image source={PostImg5} style={styles.imagePostProfile}>
 
                                     </Image>
                                 </View>
@@ -94,7 +108,7 @@ export default function Profile() {
                         <TouchableOpacity style={styles.buttonPost}>
                             <View>
                                 <View style={"#"}>
-                                    <Image>
+                                    <Image source={PostImg6} style={styles.imagePostProfile}>
 
                                     </Image>
                                 </View>
@@ -103,7 +117,7 @@ export default function Profile() {
                         <TouchableOpacity style={styles.buttonPost}>
                             <View>
                                 <View style={"#"}>
-                                    <Image>
+                                    <Image source={PostImg2} style={styles.imagePostProfile}>
 
                                     </Image>
                                 </View>
@@ -112,7 +126,7 @@ export default function Profile() {
                         <TouchableOpacity style={styles.buttonPost}>
                             <View>
                                 <View style={"#"}>
-                                    <Image>
+                                    <Image source={PostImg3} style={styles.imagePostProfile}>
 
                                     </Image>
                                 </View>
@@ -121,7 +135,34 @@ export default function Profile() {
                         <TouchableOpacity style={styles.buttonPost}>
                             <View>
                                 <View style={"#"}>
-                                    <Image>
+                                    <Image source={PostImg} style={styles.imagePostProfile}>
+
+                                    </Image>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.buttonPost}>
+                            <View>
+                                <View style={"#"}>
+                                    <Image source={PostImg4} style={styles.imagePostProfile}>
+
+                                    </Image>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.buttonPost}>
+                            <View>
+                                <View style={"#"}>
+                                    <Image source={PostImg5} style={styles.imagePostProfile}>
+
+                                    </Image>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.buttonPost}>
+                            <View>
+                                <View style={"#"}>
+                                    <Image source={PostImg6} style={styles.imagePostProfile}>
 
                                     </Image>
                                 </View>
@@ -129,6 +170,7 @@ export default function Profile() {
                         </TouchableOpacity>
                     </View>
                 </ScrollView>
+
             </View>
         </SafeAreaView>
     );
